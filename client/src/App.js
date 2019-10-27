8938aad5d63715d9a6eb9b18f36dc3bf4f2058e9
Vinicius <vinicius.bonifacio1985@gmail.com>
2019-10-27 09:45:45 +0200

    Create the reducers to deal with the previous state and the actions

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import AppNavbar from "./components/AppNavbar"
 import ShoppingList from "./components/ShoppingList";
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import { Provider} from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,10 @@
+import uuid from "uuid";
+
+const initialState = {
+    items: [
+        { id: uuid(), name: "Cookies"},
+        { id: uuid(), name: "Chocolate"},
+        { id: uuid(), name: "Brownies"},
+        { id: uuid(), name: "Coxinha"}
+    ]
+};
