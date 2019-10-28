582a3455a78e27f2b73bc9c458ed8b243031518a
Vinicius <vinicius.bonifacio1985@gmail.com>
2019-10-28 16:52:59 +0200

    Implement both delete and add items features fetching data from MongoDB and getting rid of the hard-coded static data

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -11,18 +11,20 @@ export const getItems = () => dispatch => {
             }))
 };
 
-export const deleteItem = id => {
-    return {
-        type: DELETE_ITEM,
-        payload: id
-    };
+export const deleteItem = id => dispatch => {
+    axios.delete(`/api/items/${id}`)
+        .then(res => dispatch({
+            type: DELETE_ITEM,
+            payload: id
+        }));
 };
 
-export const addItem = item => {
-    return {
-        type: ADD_ITEM,
-        payload: item
-    };
+export const addItem = item => dispatch => {
+    axios.post("/api/items", item)
+        .then(res => dispatch({
+            type: ADD_ITEM,
+            payload: res.data
+        }));
 };
 
 export const setItemsLoading = () => {
--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,7 +11,6 @@ import {
 } from "reactstrap";
 import { connect } from "react-redux";
 import { addItem } from "../actions/itemActions";
-import uuid from "uuid";
 
 class ItemModal extends Component {
     state = {
@@ -33,7 +32,6 @@ class ItemModal extends Component {
         e.preventDefault();
 
         const newItem = {
-            id: uuid(),
             name: this.state.name
         };
 
