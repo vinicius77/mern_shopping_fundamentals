0fbb86d0cf42807ba9c73ce9ece16f0efe2ec310
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-01-07 23:38:53 +0200

    feat(upgrade) upgrading deprecated dependencies

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-// This file is longer in use in our app
-
-module.exports = {
-    mongoURI: "This URI must come from the cloud.mongodb connection string"// This URI comes from the cloud.mongodb.com account (Configure JWT to avoid expose your sensible data)
-};
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path"); //Core nodejs module
-const config = require("config");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -10,26 +10,28 @@ app.use(express.json());
 
 // DB Config imported from the keys.js file
 //const db = config.mongoURI; *Also worked
-const db = config.get("mongoURI");
+const db = process.env.MONGO_DB_URI;
+
+//config.get("mongoURI");
 
 // Connecting to Mongo DB
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-        .then(() => console.log("Successful Conected on MongoDB!"))
-        .catch(err => console.log(err));
+mongoose
+	.connect(db)
+	.then(() => console.log('🚀🚀 Successfully Conected to MongoDB! 🚀🚀'))
+	.catch((err) => console.log(err));
 
 //Use Routes
-app.use("/api/items", require("./routes/api/items"));
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in PRODUCTION
-if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static("client/build"));
+	app.use(express.static('client/build'));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
