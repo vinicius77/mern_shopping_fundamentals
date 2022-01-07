3c89bde012ab4ae2990aacd6593a80f4efa425c5
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2022-01-07 23:51:07 +0200

    feat(upgrade) removes config dependency, solves routes warnings

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,50 +1,57 @@
-import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL,
-        LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL} from "../actions/types";
+import {
+	USER_LOADED,
+	USER_LOADING,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT_SUCCESS,
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+} from '../actions/types';
 
 const initialState = {
-    token: localStorage.getItem("token"),
-    isAuthenticated: null,
-    isLoading: null,
-    user: null
+	token: localStorage.getItem('token'),
+	isAuthenticated: null,
+	isLoading: null,
+	user: null,
 };
 
-export default function(state= initialState, action){
-    switch(action.type){
-        case USER_LOADING:
-            return {
-                ...state,
-                isLoading: true
-            };
-        case USER_LOADED:
-            return {
-                ...state,
-                isAuthenticated: true,
-                isLoading: true,
-                user: action.payload
-        };
-        case LOGIN_SUCCESS:
-        case REGISTER_SUCCESS:
-            localStorage.setItem("token", action.payload.token);
-            return {
-                ...state,
-                ...action.payload,
-                isAuthenticated: true,
-                isLoading: false
-            };
-        case AUTH_ERROR:
-        case LOGIN_FAIL:
-        case LOGOUT_SUCCESS:
-        case REGISTER_FAIL:
-            localStorage.removeItem("token");
-            return {
-                ...state,
-                isAuthenticated: false,
-                token: null,
-                user: null,
-                isLoading: false
-            };
-        default: 
-            return state;
-    }
-
+export default function authReducer(state = initialState, action) {
+	switch (action.type) {
+		case USER_LOADING:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case USER_LOADED:
+			return {
+				...state,
+				isAuthenticated: true,
+				isLoading: true,
+				user: action.payload,
+			};
+		case LOGIN_SUCCESS:
+		case REGISTER_SUCCESS:
+			localStorage.setItem('token', action.payload.token);
+			return {
+				...state,
+				...action.payload,
+				isAuthenticated: true,
+				isLoading: false,
+			};
+		case AUTH_ERROR:
+		case LOGIN_FAIL:
+		case LOGOUT_SUCCESS:
+		case REGISTER_FAIL:
+			localStorage.removeItem('token');
+			return {
+				...state,
+				isAuthenticated: false,
+				token: null,
+				user: null,
+				isLoading: false,
+			};
+		default:
+			return state;
+	}
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,62 +1,59 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const config = require("config");
-const jwt = require("jsonwebtoken");
-const auth = require("../../middleware/auth");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth = require('../../middleware/auth');
 
-//User model 
-const User = require("../../models/User");
+//User model
+const User = require('../../models/User');
 
 // @route POST api/auth
 // @desc Authenticate the user
 // @access Public
-router.post("/", (req, res) => {
-    
-    const { email, password} = req.body; 
+router.post('/', (req, res) => {
+	const { email, password } = req.body;
 
-    //Simple Validation
-    if(!email || !password) {
-        return res.status(400).json({ msg: "All Fields Are Required" });
-    }
+	//Simple Validation
+	if (!email || !password) {
+		return res.status(400).json({ msg: 'All Fields Are Required' });
+	}
 
-    // Checking for not existing user
-    User.findOne({ email })
-        .then(user => {
-            if(!user) return res.status(400).json({ msg: "User Doesn't Exist!"});
+	// Checking for not existing user
+	User.findOne({ email }).then((user) => {
+		if (!user) return res.status(400).json({ msg: "User Doesn't Exist!" });
 
-            // Validate Password
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if(!isMatch) return res.status(400).json({ msg: "Invalid Password"});
+		// Validate Password
+		bcrypt.compare(password, user.password).then((isMatch) => {
+			if (!isMatch) return res.status(400).json({ msg: 'Invalid Password' });
 
-                    jwt.sign(
-                        { id: user.id },
-                        config.get("jwtSecret"),
-                        /*{ expiresIn: 3600},*/
-                        (error, token) => {
-                            if(error) throw error;
-                            res.json({
-                                token,
-                                user: {
-                                    id: user.id,
-                                    name: user.name,
-                                    email: user.email
-                                }
-                            })
-                        }
-                    )
-                })            
-        });
+			jwt.sign(
+				{ id: user.id },
+				process.env.JWT,
+				/*{ expiresIn: 3600},*/
+				(error, token) => {
+					if (error) throw error;
+					res.json({
+						token,
+						user: {
+							id: user.id,
+							name: user.name,
+							email: user.email,
+						},
+					});
+				}
+			);
+		});
+	});
 });
 
 // @route GET api/auth/user
 // @desc Get the user data
 // @access Public / Protected
-router.get("/user", auth, (req, res) => {
-    User.findById(req.user.id)
-        .select("-password")
-        .then(user => res.json(user));
-})
+router.get('/user', auth, (req, res) => {
+	User.findById(req.user.id)
+		.select('-password')
+		.then((user) => res.json(user));
+});
 
 module.exports = router;
